c4390b10861afb0a1e285ec46466e43c6766aa2f
Martini Arialdo <arialdo.martini@tcpos.com>
2018-06-20 14:24:24 +0200

    An empty project with a passing, dummy test

---
+++ src/Linquiz/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Linquiz
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void should_pass()
+        {
+            "friends".Should().Be("friends");
+        }
+    }
+}
